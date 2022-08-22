c4397fff9316f3d0f51b52bdf52b06c6f9df8437
Rahim Ratnani <rahimratnani4@gmail.com>
2022-08-22 12:47:48 +0530

    Revert "Initial commit"
    This reverts commit 028b8130c157318564643387dbd9dee1106bce63.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,33 +1,8 @@
-import '@/css/tailwind.css'
-import '@/css/prism.css'
-import 'katex/dist/katex.css'
-
-import '@/css/docsearch.css'
-import '@docsearch/css'
-
-import '@fontsource/inter/variable-full.css'
-
-import { ThemeProvider } from 'next-themes'
+import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
 
-import siteMetadata from '@/data/siteMetadata'
-import Analytics from 'pliny/analytics'
-import { SearchProvider } from 'pliny/search'
-import LayoutWrapper from '@/components/LayoutWrapper'
-
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
-      <Head>
-        <meta content="width=device-width, initial-scale=1" name="viewport" />
-      </Head>
-      <Analytics analyticsConfig={siteMetadata.analytics} />
-      <SearchProvider searchConfig={siteMetadata.search}>
-        <LayoutWrapper>
-          <Component {...pageProps} />
-        </LayoutWrapper>
-      </SearchProvider>
-    </ThemeProvider>
-  )
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
+
+export default MyApp
--- scripts/search.mjs
+++
@@ -1,5 +0,0 @@
-import { writeFileSync } from 'fs'
-import { allCoreContent } from 'pliny/utils/contentlayer.mjs'
-import { allBlogs } from '../.contentlayer/generated/index.mjs'
-
-writeFileSync('public/search.json', JSON.stringify(allCoreContent(allBlogs)))
