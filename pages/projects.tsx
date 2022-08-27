e86bf987c13998917d6bd47efaf521ffc5339c20
Rahim Ratnani <rahimratnani4@gmail.com>
2022-08-27 15:11:45 +0530

    Add projects page

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -4,7 +4,13 @@ import Layout from '../components/layout';
 const Projects: NextPage = () => {
   return (
     <Layout>
-      <h1>Projects</h1>
+      <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16 w-full">
+        <div className="space-y-2 pt-6 pb-8 md:space-y-5 border-b-stone-200 dark:border-b-stone-700 border-b w-full">
+          <h1 className="text-3xl font-bold leading-9 tracking-tight text-light-theme-1 dark:text-dark-theme-1 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
+            Projects
+          </h1>
+        </div>
+      </div>
     </Layout>
   );
 };
