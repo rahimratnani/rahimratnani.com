b21d04bbbc61aac4f625debeaf8baf5ac66b7d82
Rahim Ratnani <rahimratnani4@gmail.com>
2022-09-09 16:49:48 +0530

    ""

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -115,40 +115,6 @@ export default function Layout(props: LayoutProps) {
             </a>
           </div>
           {/* Dark mode button */}
-          {/* <button
-            aria-label="Toggle Dark Mode"
-            type="button"
-            className="w-9 h-9 bg-zinc-200 rounded-lg dark:bg-zinc-600 flex items-center justify-center  hover:ring-2 ring-zinc-400 transition-all"
-            onClick={() => {
-              setTheme(resolvedTheme === 'dark' ? 'light' : 'dark');
-            }}
-          >
-            {mounted && (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                className="w-5 h-5 text-zinc-800 dark:text-zinc-200"
-              >
-                {resolvedTheme === 'dark' ? (
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-                  />
-                ) : (
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                  />
-                )}
-              </svg>
-            )}
-          </button> */}
 
           <button
             aria-label="Toggle Dark Mode"
